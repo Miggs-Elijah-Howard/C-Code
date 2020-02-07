0241c121a966e1002e0480f2485734ffbfacff3c
Miggs-Elijah-Howard <58825116+Miggs-Elijah-Howard@users.noreply.github.com>
2020-02-07 12:40:03 -0700

    Update BreakCompAdvantage.cpp

--- BreakCompAdvantage.cpp
+++ BreakCompAdvantage.cpp
@@ -2,55 +2,50 @@
 #include <ctime>
 #include <cstdlib>
 using namespace std;
-typedef long long ll;
-double MattOppCostFood(double& foodProdMatt, double& techProdMatt){
+typedef unsigned long li;
+float MattOppCostFood(float& foodProdMatt, float& techProdMatt){
     return techProdMatt / foodProdMatt;
 }
-double MattOppCostTech(double& foodProdMatt, double& techProdMatt){
+float MattOppCostTech(float& foodProdMatt, float& techProdMatt){
     return foodProdMatt / techProdMatt;
 }
-double RyanOppCostFood(double& foodProdRyan, double& techProdRyan){
+float RyanOppCostFood(float& foodProdRyan, float& techProdRyan){
     return techProdRyan / foodProdRyan;
 }
-double RyanOppCostTech(double& foodProdRyan, double& techProdRyan){
+float RyanOppCostTech(float& foodProdRyan, float& techProdRyan){
     return foodProdRyan / techProdRyan;
 }
 int main(){
-    ll halfWayMark = 0;
+    srand(time(0));
+    long int halfWayMark = 0;
     bool broken = false;
-    while(true) {
-        for(ll i = 0; i < 1000000000; i++) {
-            srand(time(0));
-            double foodProdMatt = rand();
-            double techProdMatt = rand();
-            double foodProdRyan = rand();
-            double techProdRyan = rand();
+    for(long int i = 0; i < 1000000000000; i++) {
+        float foodProdMatt = rand();
+        float techProdMatt = rand();
+        float foodProdRyan = rand();
+        float techProdRyan = rand();
 
-            if (MattOppCostFood(foodProdMatt, techProdMatt) < RyanOppCostFood(foodProdRyan, techProdRyan) &&
-            MattOppCostTech(foodProdMatt, techProdMatt) < RyanOppCostTech(foodProdRyan, techProdRyan)){
-                cout << "Broken!" << endl;
-                cout << "Matt's OppCostTech: " << MattOppCostTech(foodProdMatt, techProdMatt) << endl;
-                cout << "Matt's OppCostFood: " << MattOppCostFood(foodProdMatt, techProdMatt) << endl;
-                cout << "Ryan's OppCostTech: " << RyanOppCostTech(foodProdRyan, techProdRyan) << endl;
-                cout << "Ryan's OppCostFood: " << RyanOppCostFood(foodProdRyan, techProdRyan) << endl;
-            }
-            else if (MattOppCostFood(foodProdMatt, techProdMatt) > RyanOppCostFood(foodProdRyan, techProdRyan) &&
-            MattOppCostTech(foodProdMatt, techProdMatt) > RyanOppCostTech(foodProdRyan, techProdRyan)){
-                cout << "Broken!" << endl;
-                cout << "Matt's OppCostTech: " << MattOppCostTech(foodProdMatt, techProdMatt) << endl;
-                cout << "Matt's OppCostFood: " << MattOppCostFood(foodProdMatt, techProdMatt) << endl;
-                cout << "Ryan's OppCostTech: " << RyanOppCostTech(foodProdRyan, techProdRyan) << endl;
-                cout << "Ryan's OppCostFood: " << RyanOppCostFood(foodProdRyan, techProdRyan) << endl;
-            }
-            ++halfWayMark;
-            if (1000000000.0/halfWayMark == 2) cout << "Halfway there..." << endl;
-            cout << halfWayMark << endl;
+        if (MattOppCostFood(foodProdMatt, techProdMatt) < RyanOppCostFood(foodProdRyan, techProdRyan) &&
+            MattOppCostTech(foodProdMatt, techProdMatt) < RyanOppCostTech(foodProdRyan, techProdRyan)) {
+            cout << "Broken!" << endl;
+            cout << "Matt's OppCostTech: " << MattOppCostTech(foodProdMatt, techProdMatt) << endl;
+            cout << "Matt's OppCostFood: " << MattOppCostFood(foodProdMatt, techProdMatt) << endl;
+            cout << "Ryan's OppCostTech: " << RyanOppCostTech(foodProdRyan, techProdRyan) << endl;
+            cout << "Ryan's OppCostFood: " << RyanOppCostFood(foodProdRyan, techProdRyan) << endl;
+            broken = true;
+        } else if (MattOppCostFood(foodProdMatt, techProdMatt) > RyanOppCostFood(foodProdRyan, techProdRyan) &&
+                   MattOppCostTech(foodProdMatt, techProdMatt) > RyanOppCostTech(foodProdRyan, techProdRyan)) {
+            cout << "Broken!" << endl;
+            cout << "Matt's OppCostTech: " << MattOppCostTech(foodProdMatt, techProdMatt) << endl;
+            cout << "Matt's OppCostFood: " << MattOppCostFood(foodProdMatt, techProdMatt) << endl;
+            cout << "Ryan's OppCostTech: " << RyanOppCostTech(foodProdRyan, techProdRyan) << endl;
+            cout << "Ryan's OppCostFood: " << RyanOppCostFood(foodProdRyan, techProdRyan) << endl;
+            broken = true;
         }
-        cout << "Finished? y/n: ";
-        string response;
-        cin >> response;
-        if (response == "y") break;
-        else continue;
+        ++halfWayMark;
+        if (1000000000000.0 / halfWayMark == 2) cout << "Halfway there..." << endl;
+        if ((1000000000000.0 / 2) / 2 == i) cout << "Quarter finished..." << endl;
     }
-    cout << "Is this broken? " << broken << endl;
+    if(broken) cout << "BROKEN";
+    else if (!broken) cout << "NOT BROKEN";
 }
